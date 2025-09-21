3009a8ee0ac19181bb8099c7554500af8784e519
Aritra Maity <aritra2004maity@gmail.com>
2025-09-21 07:44:39 +0000

    make this the fav icon of nextstep

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -7,16 +7,18 @@ export const Logo = ({ className }: { className?: string }) => (
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <path
-      d="M4 19V16H7V13H10V10H13V7H16V4H19"
-      stroke="hsl(var(--foreground))"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
+    <path 
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M5 21V3H8.33333L18.6667 15.3333V21H16V17.3333L8.33333 9.66667V21H5Z" 
+      fill="currentColor" 
     />
-    <path
-      d="M16 4H19V7H16V4Z"
-      fill="currentColor"
+    <path 
+      d="M8 17V15H10V13H12V11H14V9H16V7H18V5L20 7V5H18" 
+      stroke="hsl(var(--background))"
+      strokeWidth="1.5" 
+      strokeLinecap="round" 
+      strokeLinejoin="round" 
     />
   </svg>
 );
