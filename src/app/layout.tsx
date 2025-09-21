c7c233f3e475ac90406eb14593796a4514d1dc06
Aritra Maity <aritra2004maity@gmail.com>
2025-09-21 12:04:07 +0000

    can u use the same logo for when the website is hoste on the chrome tabs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,24 +36,3 @@ export default function RootLayout({
     </html>
   );
 }
-
-export function Favicon() {
-    return (
-      <svg
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M4 20V16H8V12H12V8H16V4H20"
-          stroke="#6973FF"
-          strokeWidth="3"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-        <rect x="16" y="2" width="5" height="5" rx="1" fill="#6973FF" />
-      </svg>
-    );
-  }
